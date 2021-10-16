66279899e07f1a5c2abe48e7be4e64ba0ca0660a
liushihai1987 <394752557@qq.com>
2021-10-16 19:47:33 +0800

    2

---
+++ DEBUG V4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+from ftplib import FTP
+import os,sys
+import configparser
+#打开FTP，用于检测是否存在规格书等文件
+
+config = configparser.ConfigParser()
+config.read('cfg.ini')
+section="ftp"
+FTP_IP=config.get(section,"IP")
+FTP_PORT=config.get(section,"Port")
+FTP_User=config.get(section,"User")
+FTP_PSW=config.get(section,"PSW")
+
+ftp=FTP()
+print("调试2")
+ftp.connect(FTP_IP,int(FTP_PORT))
+ftp.login(FTP_User,FTP_PSW) #"Adminitrator","\'"
+#ftp.cwd( '/datasheets' )
+dataname=ftp.nlst()
+ftp.quit()
+ftp.quit()
+ftp.quit()
